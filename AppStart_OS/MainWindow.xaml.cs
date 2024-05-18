b970d29c0393fa508b67fe983a0a770f21148970
Aleksandr Petrov <vigasurt@gmail.com>
2024-05-18 13:58:21 +0300

    первый ком

---
+++ AppStart_OS/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System;
+
+namespace AppStart_OS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("shell32.dll")]
+
+        public static extern int ShellExecute(int hWnd, string lpOperation, string lpFile, string lpParameters, string lbDirectory, int snShowCmd);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenApp_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ShellExecute(0, "Open", tbAppName.Text, "","",1);
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
